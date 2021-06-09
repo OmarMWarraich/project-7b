8a2e4286b42b9b0a0bf77616a5ca69df1c1fe05c
OmarMWarraich <earthling986@gmail.com>
2021-06-09 07:54:05 -0400

    Ignore changes in App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/hours/i)).toBeInTheDocument();
+  });
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import CountDownTimer from './CountDownTimer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <CountDownTimer hours={1} minutes={20} seconds={40}/>
+      </div>
     </div>
   );
 }
